8159abaccfa887dcf0249ee176efb46be378132a
Denis <mod.amdx4@gmail.com>
2024-01-03 02:40:38 +0200

    If...else statement

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-// 1. Create two variables, firstCard and secondCard. 
-// Set their values to a random number between 2-11
-let firstCard = 4
+let firstCard = 13
 let secondCard = 10
-// 2. Create a variable, sum, and set it to the sum of the two cards
 let sum = firstCard + secondCard
-console.log(sum)
+
+if (sum < 21) {
+    console.log("Do you want to draw a new card? 🙂")
+}
+else if (sum === 21) {
+    console.log("Wohoo! You've got Blackjack! 🥳")
+}
+else if (sum > 21) {
+    console.log("You're out of the game! 😭")
+}
