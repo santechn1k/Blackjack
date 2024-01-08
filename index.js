4c0c08bab6d0643626ab18268e3c06081b33103a
Denis <mod.amdx4@gmail.com>
2024-01-08 03:22:09 +0200

    Adding hasBlackJack variable

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-let firstCard = 13
+let firstCard = 11
 let secondCard = 10
 let sum = firstCard + secondCard
+let hasBlackJack = false
 
-if (sum < 21) {
+if (sum <= 20) {
     console.log("Do you want to draw a new card? 🙂")
 }
 else if (sum === 21) {
     console.log("Wohoo! You've got Blackjack! 🥳")
+    hasBlackJack = true
 }
 else if (sum > 21) {
     console.log("You're out of the game! 😭")
 }
+
+
+//CASH OUT!
+console.log(hasBlackJack)
